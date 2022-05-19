52f4a94477414897f2c5d7faf6540ba1d36ef5ab
alieyuib <alieyuib@gmail.com>
2022-05-19 12:18:46 +0100

    appjs001

--- public/app-assets/js/app.js
+++ public/app-assets/js/app.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
-    $("#loginBtn").on("click", function (e) {
-        e.preventDefault();
-        var formData = {
-            title: $("#username").val(),
-            description: $("#password").val(),
-        };
-        console.log(formData["description"]);
-    });
+    // $("#loginBtn").on("click", function (e) {
+    //     e.preventDefault();
+    //     var formData = {
+    //         title: $("#username").val(),
+    //         description: $("#password").val(),
+    //     };
+    //     console.log(formData["description"]);
+    // });
+    alert("Wlcome");
 });

7ada1982a636b3492609aba4ef5e47ecd7a34711
alieyuib <alieyuib@gmail.com>
2022-05-19 12:23:29 +0100

    main.blade

--- public/app-assets/js/app.js
+++ public/app-assets/js/app.js
@@ -1,11 +1,10 @@
 $(document).ready(function () {
-    // $("#loginBtn").on("click", function (e) {
-    //     e.preventDefault();
-    //     var formData = {
-    //         title: $("#username").val(),
-    //         description: $("#password").val(),
-    //     };
-    //     console.log(formData["description"]);
-    // });
-    alert("Wlcome");
+    $("#loginBtn").on("click", function (e) {
+        e.preventDefault();
+        var formData = {
+            title: $("#username").val(),
+            description: $("#password").val(),
+        };
+        alert(formData["description"]);
+    });
 });

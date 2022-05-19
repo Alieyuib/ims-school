bbaa126f16916c246dd449eb36ae604dbf023f4b
alieyuib <alieyuib@gmail.com>
2022-05-19 12:40:07 +0100

    login_screen

--- public/app-assets/js/app.js
+++ public/app-assets/js/app.js
@@ -1,13 +1,31 @@
 $(document).ready(function () {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener("mouseenter", Swal.stopTimer);
+            toast.addEventListener("mouseleave", Swal.resumeTimer);
+        },
+    });
     $("#loginBtn").on("click", function (e) {
         e.preventDefault();
         var formData = {
             title: $("#username").val(),
             description: $("#password").val(),
         };
-        Swal.fire({
-            title: "Button Pressed!",
-            icon: "success",
-        });
+        if (formData["title"] == "" && formData["description" == ""]) {
+            Toast.fire({
+                icon: "error",
+                title: "Username/Password fields require",
+            });
+        } else {
+            Toast.fire({
+                icon: "success",
+                title: "Signed in successfully",
+            });
+        }
     });
 });
